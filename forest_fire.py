af661b740324fb11db41945689a8ecb8b6021eff
SHANJAY975 <shanjay21cs082@jit.ac.in>
2022-09-09 09:28:44 +0530

    CREATED A WEB APP

--- forest_fire.py
+++ forest_fire.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import warnings
 import pickle
+
 warnings.filterwarnings("ignore")
 
 data = pd.read_csv("Forest_fire.csv")
@@ -19,16 +20,12 @@ X = X.astype('int')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 log_reg = LogisticRegression()
 
-
 log_reg.fit(X_train, y_train)
 
-inputt=[int(x) for x in "45 32 60".split(' ')]
-final=[np.array(inputt)]
+inputt = [int(x) for x in "45 32 60".split(' ')]
+final = [np.array(inputt)]
 
 b = log_reg.predict_proba(final)
 
-
-pickle.dump(log_reg,open('model.pkl','wb'))
-model=pickle.load(open('model.pkl','rb'))
-
-
+pickle.dump(log_reg, open('model.pkl', 'wb'))
+model = pickle.load(open('model.pkl', 'rb'))
